8a11c8808a5a661de171d913fdb80ef83df22a82
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2019-07-30 18:02:57 -0400

    add basic form

---
+++ src/Form.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Form = () => {
+    return(
+        <form>
+            <label htmlFor="owner">Owner</label>
+            <input type="text" name="owner" id="owner" />
+            <label htmlFor="description">Description</label>
+            <input
+                type="text"
+                name="description"
+                id="description"
+            />
+            <input type="button" value="Add New Cat" />
+            <input type="submit" value="Submit" />
+        </form>
+    )
+};
+
+export default Form;

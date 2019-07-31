35cfd617e7ebcf2177cf3821d73335fd2e2e0227
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2019-07-30 21:26:33 -0400

    add dynamic aspect

--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Form = () => {
+    const blankCat = { name: '', age: '' };
+    const [catState, setCatState] = useState([
+        blankCat,
+    ]);
+
+    const addCat = () => {
+        setCatState([...catState, blankCat]);
+    };
+
     return (
         <form>
             <label htmlFor="owner">Owner</label>
@@ -11,7 +20,37 @@ const Form = () => {
                 name="description"
                 id="description"
             />
-            <input type="button" value="Add New Cat" />
+            <input
+                type="button"
+                value="Add New Cat"
+                onClick={addCat}
+            />
+            {
+                catState.map((val, idx) => {
+                    const catId = `name-${idx}`;
+                    const ageId = `age-${idx}`;
+                    return (
+                        <div key={`cat-${idx}`}>
+                            <label htmlFor={catId}>{`Cat #${idx + 1}`}</label>
+                            <input
+                                type="text"
+                                name={catId}
+                                data-id={idx}
+                                id={catId}
+                                className="name"
+                            />
+                            <label htmlFor={ageId}>Age</label>
+                            <input
+                                type="text"
+                                name={ageId}
+                                data-id={idx}
+                                id={ageId}
+                                className="age"
+                            />
+                        </div>
+                    );
+                })
+            }
             <input type="submit" value="Submit" />
         </form>
     );
